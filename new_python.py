5c0e9f3a69e5b61b4401e21842eef51fd4afefe1
MettaTronPrime <88951411+MettaTronPrime@users.noreply.github.com>
2021-09-30 19:08:29 -0400

    Add files via upload

---
+++ new_python.py
@@ -0,0 +1,9 @@
+#First Code- Coffee Robot Barista
+
+print ("Hello, Welcome to the Asiatic Black Coffeeshop!!!! \n")
+
+name = input("Can I please have your name?")
+
+print ("Hello, " + name + "\nThank you so much for coming in today!!")
+
+
